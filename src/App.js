fe91dd49fd0209eb1e56a46167e512ffa77b4eaf
Trung Vu <trungvh6@fpt.com.vn>
2021-07-21 20:40:35 +0700

    update test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vu Hieu Trung
         </a>
       </header>
     </div>
